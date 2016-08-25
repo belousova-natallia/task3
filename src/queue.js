da6ddb2650ed9c7c2e6da90168e1f980f3c6ea51
belousova-natallia <belousova_natallia@tut.by>
2016-08-26 00:39:37 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,75 @@
+
 const MaxHeap = require('./max-heap.js');
+
+class PriorityQueue {
+
+	constructor(maxSize) {
+		if(maxSize){
+	this.maxSize = maxSize;
+} else {
+	this.maxSize = 30;
+
+
+};
+this.heap = new MaxHeap();
+//this.arrayQueue = [];
+	}
+
+	push(data, priority) {
+if(this.heap.arrayNodes.length === this.maxSize) {
+	throw new Error;
+};
+
+this.heap.push(data, priority);
+
+	}
+
+	shift() {
+if(this.heap.arrayNodes.length === 0){
+	throw new Error;
+}
+
+
+//var arrayForShift = this.heap.arrayNodes.slice();
+
+function SortByPriority(a, b){
+	if(a.priority <= b.priority) return 1;
+	else return -1;
+}
+
+ this.heap.arrayNodes.sort(SortByPriority);
+
+
+//this.heap.arrayNodes = this.heap.arrayNodes.slice(0, this.heap.arrayNodes.length -2 );
+ this.heap.pop();
+
+ //this.heap.arrayNodes = this.heap.arrayNodes.slice(1);
+
+ //for(var i = 0; i < this.heap.arrayNodes.length; i++){
+//dataByPriority[i] = arrayForShift[i].data;
+
+ //};
+ //this.heap.pop();
+
+
+//this.heap.arrayNodes.unshift(this.heap.arrayNodes[this.heap.arrayNodes.length -1]);
+//this.heap.arrayNodes.pop();
+return this.heap.detachRoot().data;
+
+	}
+
+	size() {
+return this.heap.arrayNodes.length;
+	}
+
+	isEmpty() {
+		if (this.heap.arrayNodes.length === 0) return true;
+		else return false;
+		
+	}
+}
+
+module.exports = PriorityQueue;const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
